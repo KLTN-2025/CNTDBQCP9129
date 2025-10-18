e79844c3836163af84b0d2893f32eb580f3b3493
Datdev28 <nguyenquangdat2842003@gmail.com>
2025-10-19 01:10:52 +0700

    frontend: blog component và MyStorySection

---
+++ frontend/src/components/Blog.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div className="relative flex w-full flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+      <div className="relative mx-4 -mt-6 h-50 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
+        <img src="/story1.jpg" alt="hình ảnh mô tả câu chuyện" className="object-cover w-full h-full" />
+      </div>
+      <div className="p-6">
+        <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+          Tailwind card
+        </h5>
+        <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis
+          ligula.
+        </p>
+      </div>
+      <div className="p-6 pt-0">
+      <button className="learn-more btn-read-more">
+        <span className="circle" aria-hidden="true">
+        <span className="icon arrow"></span>
+        </span>
+        <span className="button-text">Đọc thêm</span>
+      </button>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
