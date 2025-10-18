080b94b641d0484ed04014cb935dc9d2286930c1
Datdev28 <nguyenquangdat2842003@gmail.com>
2025-10-19 00:17:04 +0700

    frontend: card component và BestSellerSection

--- frontend/src/page/home/HomePage.jsx
+++ frontend/src/page/home/HomePage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Banner from './Banner'
 import IntroduceSection from './IntroduceSection'
 import ViewShopSection from './ViewShopSection'
+import BestSeller from './BestSeller'
 const HomePage = () => {
   return (
     <div className='w-full min-h-screen flex flex-col bg-secondaryColor '>
       <Banner/>
       <IntroduceSection/>
+      <BestSeller/>
       <ViewShopSection/>
     </div>
   )
