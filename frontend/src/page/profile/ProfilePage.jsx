1ff68b9a03615fb780479dc1f64e0ee062229082
Datdev28 <nguyenquangdat2842003@gmail.com>
2025-10-20 21:04:05 +0700

    frontend: thêm component chỉ đường dẫn

--- frontend/src/page/profile/ProfilePage.jsx
+++ frontend/src/page/profile/ProfilePage.jsx
@@ -1,16 +1,20 @@
 import { Link, Outlet, useLocation } from "react-router-dom";
 import UserInfo from "../profile/UserInfo";
+import useAuthStore from "../../store/authStore";
+import Breadcrumbs from "../../components/Breadcrumbs";
 const ProfilePage = () => {
   const location = useLocation();
   const activePath = location.pathname;
 
   const isRootProfile = activePath === "/profile"; 
-
+  const user = useAuthStore(state => state.user);
   return (
-    <div className="flex min-h-screen">
-      {/* Sidebar bên trái */}
-      <div className="w-1/4 bg-gray-100 p-4 border-r">
-        <ul className="space-y-3">
+    <div className="flex min-h-screen px-20">
+      <div className="w-1/4 bg-gray-100 p-4">
+       <Breadcrumbs/>
+      <h1 className="text-xl">TRANG TÀI KHOẢN</h1>
+      <p className="text-green-700 font-bold"><span className="text-black">Xin chào, </span>{user.name}!</p>
+        <ul className="space-y-3 mt-10">
           <li>
             <Link
               to="/profile"
