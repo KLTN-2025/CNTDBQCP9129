0ff15f7173ac62093dc1214f8d5f288cbf67404a
Datdev28 <nguyenquangdat2842003@gmail.com>
2025-10-20 23:00:02 +0700

    ftonend: edit css cho profilePằ và UserInfo

--- frontend/src/page/profile/UserInfo.jsx
+++ frontend/src/page/profile/UserInfo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UserInfo = ({user}) => {
   return (
-    <div>
+    <div className='space-y-3'>
       <h2 className='text-xl font-bold text-green-700'>Thông tin tài khoản</h2>
       <p><span className='font-bold'>Họ tên: </span>{user.name}</p>
       <p><span className='font-bold'>Email: </span>{user.email}</p>
