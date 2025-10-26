4b4fbf18238a9126ca0707d1588a1452758c8574
Datdev28 <nguyenquangdat2842003@gmail.com>
2025-10-26 21:19:03 +0700

    frontend: chỉnh hook kích cỡ ảnh lên 8 * 1024 * 1024

--- frontend/src/hooks/usePreviewImage.js
+++ frontend/src/hooks/usePreviewImage.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 const usePreviewImage = () => {
   const [selectedFile, setSelectedFile] = useState([]);
   const [error, setError] = useState('')
-  const maxSizeFile = 2 * 1024 * 1024;
+  const maxSizeFile = 8 * 1024 * 1024;
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     if(selectedFile.length > 2) return setError("Bạn đã đăng quá số ảnh cho phép")
