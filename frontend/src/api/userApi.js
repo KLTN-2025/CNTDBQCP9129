57252807a5fa44b55545d058103501fb8671e3a9
Datdev28 <nguyenquangdat2842003@gmail.com>
2025-11-13 13:12:32 +0700

    full: user

---
+++ frontend/src/api/userApi.js
@@ -0,0 +1,15 @@
+import axiosClient from "./axiosClient";
+
+const userApi = {
+  getAllUsers: async(router) => {
+    console.log(router);
+    const res = await axiosClient.get(`/${router}`);
+    return res.data
+  },
+  
+  updateUserRole: async(id, data) => {
+    const res = await axiosClient.patch(`/users/${id}`, data);
+    return res.data
+  }
+}
+export default userApi
