ec6f3b28154aa2f9b5dad1a72923d6f72940353b
Datdev28 <nguyenquangdat2842003@gmail.com>
2025-11-20 16:23:30 +0700

    frontend: fix count items cart

--- frontend/src/components/navbar/Cart.jsx
+++ frontend/src/components/navbar/Cart.jsx
@@ -3,6 +3,7 @@ import useCartStore from "../../store/cartStore";
 import { Link } from "react-router-dom";
 const Cart = () => {
   const cart = useCartStore((state) => state.cart);
+ const countItems = cart.reduce((sum, item) => sum + item.quantity, 0);
   return (
     <Link to="/checkout">
       <div className="border-2 border-green-700 rounded-md flex px-4 pr-10 cursor-pointer py-2 gap-x-2 items-center group">
@@ -12,7 +13,7 @@ const Cart = () => {
         </span>
         <div className="bg-gray-200 px-2 rounded-xs">
           <span className="text-green-700">
-            {cart.length}
+            {countItems}
           </span>
         </div>
       </div>
