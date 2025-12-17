e89f57e0258e22ecb4e7c17f1d4889a6088ae1f2
Datdev28 <nguyenquangdat2842003@gmail.com>
2025-12-17 09:47:34 +0700

    backend: middle router contact

--- backend/router/contact.router.js
+++ backend/router/contact.router.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import { createContact, deleteContact, getAllContacts, getReadContacts, getUnreadContacts, markAsRead } from '../controllers/contact/contact.controller.js';
-import { verifyToken, isAdminOrStaff } from '../middleware/auth.middleware.js';
+import { verifyToken, isAdmin } from '../middleware/auth.middleware.js';
 const router = express.Router();
-router.get('/', getAllContacts);
+router.get('/', verifyToken, isAdmin, getAllContacts);
 router.post('/', createContact);
-router.delete('/:id', deleteContact);
-router.put("/read/:id", markAsRead);
-router.get("/read", getReadContacts);
-router.get("/unread", getUnreadContacts);
+router.delete('/:id', verifyToken, isAdmin, deleteContact);
+router.put("/read/:id", verifyToken, isAdmin, markAsRead);
+router.get("/read", verifyToken, isAdmin, getReadContacts);
+router.get("/unread", verifyToken, isAdmin, getUnreadContacts);
 export default router
